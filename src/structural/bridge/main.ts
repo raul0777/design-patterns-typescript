e2c51e44c27b0bbbe7bce7892fd38a8eaf462f61
raul0777 <raul.gome44@gmail.com>
2022-11-30 16:42:56 -0300

    Bridge na prática

---
+++ src/structural/bridge/main.ts
@@ -0,0 +1,26 @@
+import { Radio } from './device/radio';
+import { Tv } from './device/tv';
+import { RemoteControl } from './remote-control/remote-control';
+import { RemoteControlWithVolume } from './remote-control/remote-control-with-volume';
+
+export function clientCode(
+  abstraction: RemoteControl | RemoteControlWithVolume,
+): void {
+  abstraction.togglePower(); //true
+
+  // Type Guard
+  if (!('volumeUp' in abstraction)) return;
+
+  abstraction.volumeUp(); //20
+  abstraction.volumeUp(); //30
+  abstraction.volumeDown(); //40
+}
+
+const tv = new Tv();
+const radio = new Radio();
+
+const radioRemoteControl = new RemoteControl(radio);
+clientCode(radioRemoteControl);
+
+const tvRemoteControl = new RemoteControlWithVolume(tv);
+clientCode(tvRemoteControl);
