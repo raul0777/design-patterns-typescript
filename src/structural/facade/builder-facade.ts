83e259109c156b38435e392771054e0669cdd2a3
raul0777 <raul.gome44@gmail.com>
2022-12-01 16:19:16 -0300

    Façade na prática

---
+++ src/structural/facade/builder-facade.ts
@@ -0,0 +1,26 @@
+import { MainDishBuilder } from '../../creation/builder/classes/main-dish-builder';
+import { VeganDishBuilder } from '../../creation/builder/classes/vegan-dish-builder';
+
+export class BuilderFacade {
+  //essa é a fachada para a pasta src/creation/builder/index.ts
+  private mainDishBuilder = new MainDishBuilder();
+  private veganDishBuilder = new VeganDishBuilder();
+
+  makeMeal1(): void {
+    this.mainDishBuilder.makeMeal();
+    console.log(this.mainDishBuilder.getMeal());
+    console.log(this.mainDishBuilder.getPrice());
+  }
+
+  makeMeal2(): void {
+    this.mainDishBuilder.reset();
+    const meal2 = this.mainDishBuilder.makeBeverage().getMeal();
+    console.log(meal2);
+  }
+
+  makeMeal3(): void {
+    const veganMeal = this.veganDishBuilder.makeMeal().getMeal();
+    console.log(veganMeal);
+    console.log(veganMeal.getPrice());
+  }
+}
